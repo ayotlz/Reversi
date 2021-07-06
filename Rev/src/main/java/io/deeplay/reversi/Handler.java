ad42fe8b1e7b9ff2e19a6ef38384fdd367a33987
vrtlz <zltrvs@gmail.com>
2021-07-06 13:59:01 +0600

    modify Handler

--- Rev/src/main/java/io/deeplay/reversi/Handler.java
+++ Rev/src/main/java/io/deeplay/reversi/Handler.java
@@ -2,6 +2,8 @@ package io.deeplay.reversi;
 
 import io.deeplay.reversi.chip.Chip;
 import io.deeplay.reversi.chip.Color;
+import io.deeplay.reversi.exceptions.ReversiErrorCode;
+import io.deeplay.reversi.exceptions.ReversiException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,10 +11,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Handler {
-    public void beforeGame(Board board) {
+    public void beforeGame(Board board) throws ReversiException {
         if (board.getArray().length % 2 == 1) {
-//            throw new
+            throw new ReversiException(ReversiErrorCode.ODD_SIZE_BOARD);
         }
+
 
         int idx1 = board.getArray().length / 2 - 1;
         int idx2 = board.getArray().length / 2;
@@ -23,8 +26,22 @@ public class Handler {
         board.getArray()[idx2][idx2] = new Chip(Color.WHITE);
     }
 
-    public void makeStep(Board board, Color turnOrder, Cell cell) {
+    public boolean makeStep(Board board, Color turnOrder, Cell cell) {
+        // Находим все фишки противника
+        List<Cell> chipsOfOpponent = findWhiteOrBlackChips(board, turnOrder);
 
+        // Находим map(Cell *клетка цвета ходящего*: List<Cell>  *лист всех соседих пустых клеток*)
+//        findNeighborhoods(chipsOfOpponent)
+
+        // Получаем map(Cell *пустая клетка*: int *очки, которые мы получим за ход сюда*)
+//        getScoreMap()
+
+        // Бросаем исклющение если вдруг нельзя походить в данную клетку
+//      throw new Exception
+
+        // Ставим фишку, переворачиваем выигранные фишки, возвращаем true
+
+        return true;
     }
 
     private List<Cell> findWhiteOrBlackChips(Board board, Color turnOrder) {
@@ -50,20 +67,28 @@ public class Handler {
             List<Cell> tempList = new ArrayList<>();
             for (int j = -1; j < 2; j++) {
                 for (int k = -1; k < 2; k++) {
-                    Chip chip = board.getArray()[j+listOfWhiteOrBlackChips.get(i).getX()][k+listOfWhiteOrBlackChips.get(i).getY()];
-                        if (chip.getColor() == Color.NEUTRAL){
-                            Cell tempCell = new Cell(j+listOfWhiteOrBlackChips.get(i).getX(),k+listOfWhiteOrBlackChips.get(i).getY());
-                            tempList.add(tempCell);
-                        }
+                    Chip chip = board.getArray()[j + listOfWhiteOrBlackChips.get(i).getX()][k + listOfWhiteOrBlackChips.get(i).getY()];
+                    if (chip.getColor() == Color.NEUTRAL) {
+                        Cell tempCell = new Cell(j + listOfWhiteOrBlackChips.get(i).getX(), k + listOfWhiteOrBlackChips.get(i).getY());
+                        tempList.add(tempCell);
                     }
                 }
-            neighborhood.put(listOfWhiteOrBlackChips.get(i),tempList);
             }
+            neighborhood.put(listOfWhiteOrBlackChips.get(i), tempList);
+        }
         return neighborhood;
     }
-}
 
-//
-//    private int score(int x, int y) {
-//
-//    }
+    private Map<Cell, Integer> getScoreMap(Board board, Map<Cell, List<Cell>> neighborhoods) {
+        Map<Cell, Integer> score = new HashMap<>();
+        for (Map.Entry<Cell, List<Cell>> entry : neighborhoods.entrySet()) {
+            for (Cell cell : entry.getValue()) {
+                score.put(cell, 0);
+            }
+        }
+
+
+
+        return null;
+    }
+}
