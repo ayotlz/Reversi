67437c57ceb9b931c74eb7b20e5fcecad3ffb97f
KIRILL-NOTEBOOK <zabollirik@gmail.com>
2021-08-11 16:33:18 +0600

    Add Kirill package with bots and utilityFunctions

---
+++ Rev/Kirill/src/main/java/Kirill/MiniMaxBot.java
@@ -0,0 +1,143 @@
+package Kirill;
+
+import Kirill.UtilityFunctions.IFunction;
+import Kirill.UtilityFunctions.SimpleScoreFunction;
+import exceptions.ReversiException;
+import handler.Handler;
+import models.board.Board;
+import models.board.Cell;
+import models.chip.Color;
+import player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.ToDoubleFunction;
+
+public class MiniMaxBot extends Player {
+
+    private IFunction utilityFunction = new SimpleScoreFunction();
+    private final Handler handler = new Handler();
+    private double alpha = Double.MIN_VALUE;
+    private double beta = Double.MAX_VALUE;
+    private static final int MAXRECLEVEL = 5;
+
+    public MiniMaxBot(final Color color) {
+        super(color);
+        setName("KirillMiniMaxBot");
+    }
+
+    public MiniMaxBot(final Color color, final IFunction function) {
+        super(color);
+        setName("KirillMiniMaxBot "+function.toString());
+        this.utilityFunction = function;
+    }
+
+    private static final class AnswerAndWin {
+        final Cell cell;
+        final double win;
+
+        private AnswerAndWin(final Cell cell, final double win) {
+            this.cell = cell;
+            this.win = win;
+        }
+
+        @Override
+        public final String toString() {
+            return cell.toString() + " " + win;
+        }
+    }
+
+    @Override
+    public final Cell getAnswer(final Board board) {
+
+        final Map<Cell, List<Cell>> scoreMap = board.getScoreMap(getPlayerColor());
+        final List<AnswerAndWin> awList = new ArrayList<>();
+        final List<Cell> cellsToAct = new ArrayList<>(scoreMap.keySet());
+
+
+        for (final Cell key : cellsToAct) {
+            final Board copyBoard = new Board(board);
+            try {
+                handler.makeStep(copyBoard, key, getPlayerColor());
+            } catch (final ReversiException ignored) {
+            }
+
+            final double win = getWinByGameTree(copyBoard, 0);
+            awList.add(new AnswerAndWin(key, win));
+        }
+        return getGreedyDecision(awList, aw -> aw.win).cell;
+    }
+
+    private double getWinByGameTree(final Board board, final int recLevel) {
+
+        final Color currentPlayer = board.getNextPlayer();
+
+        ToDoubleFunction<AnswerAndWin> winCalculator;
+        if (currentPlayer == getPlayerColor()) {
+            winCalculator = (aw -> aw.win);
+        } else {
+            winCalculator = (aw -> -aw.win);
+        }
+
+        if (handler.isGameEnd(board)) {
+            return computeWin(board);
+        }
+
+        if (recLevel == MAXRECLEVEL) {
+            return getWinScore(board);
+        }
+
+        final Map<Cell, List<Cell>> scoreMap = board.getScoreMap(currentPlayer);
+        final List<AnswerAndWin> awList = new ArrayList<>();
+        final List<Cell> cellsToAct = new ArrayList<>(scoreMap.keySet());
+
+        for (final Cell key : cellsToAct) {
+            final Board copyBoard = new Board(board);
+            try {
+                handler.makeStep(copyBoard, key, currentPlayer);
+            } catch (final ReversiException ignored) {
+            }
+
+            final double win = getWinByGameTree(copyBoard, recLevel + 1);
+            if (currentPlayer == getPlayerColor()) {
+                if (win >= beta) {
+                    return win;
+                }
+                alpha = Math.max(alpha, win);
+            } else {
+                if (win <= alpha) {
+                    return win;
+                }
+                beta = Math.min(beta, win);
+            }
+            awList.add(new AnswerAndWin(key, win));
+        }
+        return getGreedyDecision(awList, winCalculator).win;
+    }
+
+    private AnswerAndWin getGreedyDecision(final List<AnswerAndWin> awList,
+                                           final ToDoubleFunction<AnswerAndWin> winCalculator) {
+        AnswerAndWin bestAW = awList.get(0);
+        double bestWin = winCalculator.applyAsDouble(bestAW);
+        for (int i = 1; i < awList.size(); i++) {
+            final AnswerAndWin currentAW = awList.get(i);
+            final double currentWin = winCalculator.applyAsDouble(currentAW);
+            if (currentWin > bestWin) {
+                bestAW = currentAW;
+                bestWin = currentWin;
+            }
+        }
+        return bestAW;
+    }
+
+    private double getWinScore(final Board board) {
+        return utilityFunction.getScore(board, getPlayerColor()) -
+                utilityFunction.getScore(board, getPlayerColor().reverseColor());
+    }
+
+    private double computeWin(final Board board) {
+        return Double.compare(utilityFunction.getScore(board, getPlayerColor()),
+                utilityFunction.getScore(board, getPlayerColor().reverseColor()));
+    }
+}
---
+++ Rev/Kirill/src/main/java/Kirill/UtilityFunctions/ExpertTableScoreFunction.java
@@ -0,0 +1,67 @@
+package Kirill.UtilityFunctions;
+
+import models.board.Board;
+import models.chip.Color;
+
+public class ExpertTableScoreFunction implements IFunction {
+    @Override
+    public final double getScore(final Board board, final Color color) {
+        double score = 0;
+        final double[][] expertBoard = getPriorityBoard(board);
+        for (int i = 0; i < board.getBoardSize(); i++) {
+            for (int j = 0; j < board.getBoardSize(); j++) {
+                if (board.getChipColor(i, j) == color) {
+                    score++;
+                    score += expertBoard[i][j];
+                }
+            }
+        }
+        return score;
+    }
+
+    private double[][] getPriorityBoard(final Board board) {
+        final double[][] priorityBoard = new double[board.getBoardSize()][board.getBoardSize()];
+        for (int i = 0; i < board.getBoardSize(); i++) {
+            for (int j = 0; j < board.getBoardSize(); j++) {
+                priorityBoard[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < board.getBoardSize(); i++) {
+            for (int j = 0; j < board.getBoardSize(); j++) {
+                if ((i == 0) || (i == board.getBoardSize() - 1)) {
+                    priorityBoard[i][j] += 5;
+                }
+                if ((j == 0) || (j == board.getBoardSize() - 1)) {
+                    priorityBoard[i][j] += 5;
+                }
+            }
+        }
+
+        priorityBoard[0][0] = 100;
+        priorityBoard[0][board.getBoardSize() - 1] = 100;
+        priorityBoard[board.getBoardSize() - 1][0] = 100;
+        priorityBoard[board.getBoardSize() - 1][board.getBoardSize() - 1] = 100;
+
+        priorityBoard[1][0] = -2;
+        priorityBoard[1][1] = -2;
+        priorityBoard[1][board.getBoardSize() - 1] = -2;
+        priorityBoard[1][board.getBoardSize() - 2] = -2;
+        priorityBoard[0][1] = -2;
+        priorityBoard[0][board.getBoardSize() - 1] = -2;
+
+        priorityBoard[board.getBoardSize() - 2][0] = -2;
+        priorityBoard[board.getBoardSize() - 2][1] = -2;
+        priorityBoard[board.getBoardSize() - 1][1] = -2;
+        priorityBoard[board.getBoardSize() - 2][board.getBoardSize() - 1] = -2;
+        priorityBoard[board.getBoardSize() - 2][board.getBoardSize() - 2] = -2;
+        priorityBoard[board.getBoardSize() - 1][board.getBoardSize() - 2] = -2;
+
+        return priorityBoard;
+    }
+
+    @Override
+    public final String toString() {
+        return "ExpertTableScoreFunction";
+    }
+}
