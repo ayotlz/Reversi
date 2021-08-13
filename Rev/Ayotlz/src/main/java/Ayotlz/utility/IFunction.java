6783aad5433cf93c884b1228d1fb42ab8f4d5bc2
vrtlz <zltrvs@gmail.com>
2021-08-13 16:26:58 +0600

    before pull request

--- Rev/Ayotlz/src/main/java/Ayotlz/utility/IFunction.java
+++
@@ -1,8 +0,0 @@
-package Ayotlz.utility;
-
-import models.board.Board;
-import models.chip.Color;
-
-public interface IFunction {
-    double getScore(final Board board, final Color playerColor, final Color turnOrder);
-}
