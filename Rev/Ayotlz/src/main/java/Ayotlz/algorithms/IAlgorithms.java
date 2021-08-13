e4ac7b4ca1d3b3938dd17b0b4094e12616578e23
vrtlz <zltrvs@gmail.com>
2021-08-13 12:23:58 +0600

    before pull request

--- Rev/Ayotlz/src/main/java/Ayotlz/algorithms/IAlgorithms.java
+++
@@ -1,9 +0,0 @@
-package Ayotlz.algorithms;
-
-import models.board.Board;
-import models.board.Cell;
-import models.chip.Color;
-
-public interface IAlgorithms {
-    Cell getCell(final Board board, final Color playerColor);
-}
