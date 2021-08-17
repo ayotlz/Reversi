409d0d628a51d2f59351376c59650c2873ddcd06
vrtlz <zltrvs@gmail.com>
2021-08-17 10:36:33 +0600

    add count of bots in config

--- Rev/Server/src/main/java/service/Service.java
+++ Rev/Server/src/main/java/service/Service.java
@@ -1,15 +1,34 @@
 package service;
 
 import clients.BotClient;
+import property.Property;
+import server.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Service {
     final Map<BotClient, Integer> bots = new HashMap<>();
+    final int countOfBots = Property.getCountOfBots();
 
     public void addBot(final BotClient botClient) {
         bots.put(botClient, 0);
+    }
+
+    public void awaitingBots(final Server server, final ServerSocket serverSocket) throws IOException {
+        for (int i = 0; i < countOfBots; i++) {
+            final Socket socket = serverSocket.accept();
+            try {
+                final BotClient bc = new BotClient(server, socket);
+                addBot(bc);
+                bc.start();
+            } catch (final IOException e) {
+                socket.close();
+            }
+        }
     }
 
     public BotClient getBot() {
